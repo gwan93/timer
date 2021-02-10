5045f86fce8ebcba6703d6b940e5cad05c959a65
vagrant <gio_wan@live.ca>
2021-02-10 03:58:23 +0000

    lint

--- timer2.js
+++ timer2.js
@@ -12,18 +12,18 @@ const numbers = {
   '\u0037': 7,
   '\u0038': 8,
   '\u0039': 9
-}
+};
 
 stdin.on('data', (key) => {
   // any numeric key
-  if (Object.keys(numbers).includes(key)) { 
-    process.std.out(`Setting timer for ${key} seconds...`)
-    setTimeout(() => process.stdout.write('\x07'), key * 1000)
+  if (Object.keys(numbers).includes(key)) {
+    process.std.out(`Setting timer for ${key} seconds...`);
+    setTimeout(() => process.stdout.write('\x07'), key * 1000);
   // press b
-  } else if (key === '\u0062') { 
+  } else if (key === '\u0062') {
     process.stdout.write('\x07');
   // ctrl+c to kill app
-  } else if (key === '\u0003') { 
+  } else if (key === '\u0003') {
     process.stdout.write('\nThanks for using me, ciao!\n');
     process.exit();
   }
